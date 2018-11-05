813e496b6170326def3e5e6cd3b763e82248bc2a
Hhpon <624791164@qq.com>
2018-11-05 17:29:59 +0800

    grunt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       repo : 'git@github.com:Hhpon/loveWall.git',
       path : '/home/dc2-user/www/website/loveWall',
       ssh_options: 'StrictHostKeyChecking=no',
-      'post-deploy': 'npm install --registry=https://registry.npm.taobao.org && grunt build && pm2 reload ecosystem.config.js --env loveWall',
+      'post-deploy': 'npm install --registry=https://registry.npm.taobao.org && pm2 reload ecosystem.config.js --env loveWall',
       env:{
         NODE_ENV: 'production'
       }
