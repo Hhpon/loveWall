5b43b4980cd2498e2628a0ed09f470c54ba0833c
Hhpon <624791164@qq.com>
2018-11-05 16:50:00 +0800

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,9 +23,10 @@ module.exports = {
       host : '117.51.157.5',
       ref  : 'origin/master',
       repo : 'git@github.com:Hhpon/loveWall.git',
-      path : '/www/website/loveWall',
+      path : '/home/dc2-user/www/website/loveWall',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy': 'npm install --registry=https://registry.npm.taobao.org && grunt build && pm2 startOrRestart ecosystem.config.js --env production',
       env:{
         NODE_ENV: 'production'
       }
