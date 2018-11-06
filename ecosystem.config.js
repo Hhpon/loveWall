1170d6830a870b4adc24c59013789443599f2de3
Hhpon <624791164@qq.com>
2018-11-06 11:52:51 +0800

    change eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,6 @@ module.exports = {
   apps : [{
     name: 'loveWall',
     script: 'app.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
