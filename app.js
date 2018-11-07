2b3414040d127475b4740f5d92fb0287514b1033
Hhpon <624791164@qq.com>
2018-11-07 19:46:35 +0800

    ignore oublic

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ app.post('/article', function (req, res) {
         console.log(docs);
     })
     res.end('我收到了');
+
+
 })
 
 app.get('/getarticle1', function (req, res) {
     bbq.find(function (err, doc) {
         console.log(doc);
         res.json(doc);
-    }).sort("-num").exec(function(err,docs){
+    }).sort("-num").exec(function (err, docs) {
         console.log(docs);
     })
 })
@@ -32,7 +34,7 @@ app.get('/getarticle2', function (req, res) {
     bbq.find({ selected: /表白/ }, function (err, doc) {
         console.log(doc);
         res.json(doc);
-    }).sort("-num").exec(function(err,docs){
+    }).sort("-num").exec(function (err, docs) {
         console.log(docs);
     })
 })
@@ -41,7 +43,7 @@ app.get('/getarticle3', function (req, res) {
     bbq.find({ selected: /一句/ }, function (err, doc) {
         console.log(doc);
         res.json(doc);
-    }).sort("-num").exec(function(err,docs){
+    }).sort("-num").exec(function (err, docs) {
         console.log(docs);
     })
 })
@@ -50,7 +52,7 @@ app.get('/getarticle4', function (req, res) {
     bbq.find({ selected: /吐槽/ }, function (err, doc) {
         console.log(doc);
         res.json(doc);
-    }).sort("-num").exec(function(err,docs){
+    }).sort("-num").exec(function (err, docs) {
         console.log(docs);
     })
 })
@@ -61,9 +63,21 @@ app.post('/getarticle5', function (req, res) {
     bbq.find({ Content: sousuo }, function (err, doc) {
         console.log(doc);
         res.json(doc);
-    }).sort("-num").exec(function(err,docs){
+    }).sort("-num").exec(function (err, docs) {
         console.log(docs);
     })
+})
+
+app.post('/removearticle', function (req, res) {
+
+    let blog = req.body.biaoji;
+    console.log(blog);
+
+    article.deleteOne({ _id: blog }, function (err, doc) {
+        console.log(doc);
+    })
+
+    res.end('我收到了');
 })
 app.listen(3001, function () {
     console.log('Example app listening on port 3001!')
--- db.js
+++ db.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
-let env = process.env.NODE_ENV || 'development'
-let dbUrl = 'mongodb://127.0.0.1:20811/loveWall'
-
-if (env === 'development') {
-    dbUrl = 'mongodb://127.0.0.1/loveWall'
-}
-
-mongoose.connect(dbUrl, { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/bbq', { useNewUrlParser: true });
 
 const articleSchema = new mongoose.Schema({
     Content: String,
-    Name: String,
-    selected: String,
-    sousuo: String,
-    num: Number,
+    Name : String,
+    selected : String,
+    sousuo : String,
+    num : Number,
+    biaoji : Number,
 })
 
-
+ 
 
 
 module.exports = mongoose.model('article', articleSchema);
