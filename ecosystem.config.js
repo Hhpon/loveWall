407e80cc4cf86cf6c95ce42066cffd74d40a9036
Hhpon <624791164@qq.com>
2018-12-02 12:04:55 +0800

    change host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
   deploy : {
     production : {
       user : 'dc2-user',
-      host : '117.51.157.5',
+      host : '117.51.158.69',
       ref  : 'origin/master',
       repo : 'git@github.com:Hhpon/loveWall.git',
       path : '/home/dc2-user/www/website/production',
