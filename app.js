bbbcd8b4e0941acffbfafebd585cf33a88fdf07b
Hhpon <624791164@qq.com>
2019-03-24 10:07:17 +0800

    fix bug

--- app.js
+++ app.js
@@ -9,25 +9,8 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(cors());
 
-//allow custom header and CORS
-// app.all("*", function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild"
-//   );
-//   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-
-//   if (req.method == "OPTIONS") {
-//     res.send(200);
-//     /让options请求快速返回/;
-//   } else {
-//     next();
-//   }
-// });
-
 app.get("/", (req, res) => {
-  req.send("hello world");
+  res.send("hello world");
 });
 
 app.post("/article", function(req, res) {
