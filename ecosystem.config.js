d34946f86422ad7a38d8772ea3a1307027bb898f
Hhpon <624791164@qq.com>
2019-03-24 10:14:56 +0800

    fix bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       host: "117.51.158.69",
       ref: "origin/master",
       repo: "git@github.com:Hhpon/loveWall.git",
-      path: "/home/dc2-hhp/www/website/production",
+      path: "/home/dc2-user/www/website/production",
       ssh_options: "StrictHostKeyChecking=no",
       "post-deploy":
         "npm install --registry=https://registry.npm.taobao.org && pm2 reload ecosystem.config.js --env production",
